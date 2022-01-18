5328c3f50c075c55ad8a77053efde4a6df954bf1
dhall-arubase <davidhall@hpe.com>
2022-01-18 17:17:48 +0000

    fixed the code by Dave.

--- git_fork_test.py
+++ git_fork_test.py
@@ -1,1 +1,8 @@
+@@ -5,5 +5,5 @@
 
+
+ #This is used to test pull request after git fork.
+ #hint: there is no function name "Print()".
+ Note: Python is a case-sensitive programming language. print() is a built-in python function.The correct format is lower case, print().
+ #this is fixed by Dave".
+ #######################################################################
